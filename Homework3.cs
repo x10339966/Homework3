e50fd7e8591c278751a7a8641438a61b4381a9be
x10339966 <patbirkeland@mac.com>
2016-07-22 17:53:14 -0700

    added the first file

---
+++ Homework3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UW_Week_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                  
+            Console.WriteLine();
+            Console.Write("Please enter a number of seconds you would like converted into days, hours, minutes and seconds:  ");
+            string stringTime = Console.ReadLine();
+            
+            int integerTime = int.Parse(stringTime);
+
+            int integerHour = integerTime / 3600;
+            integerTime = integerTime % 3600;
+            int integerMinutes = integerTime/60;
+            integerTime = integerTime % 60;
+            int integerSeconds = integerTime;
+            
+            Console.WriteLine("hours: {0} , minutes: {1}, seconds: {2}", integerHour, integerMinutes, integerSeconds);
+            Console.ReadKey();
+        }
+    }
+}
